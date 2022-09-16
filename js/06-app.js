16f6544f6801d3e5473a4456cfdd93ef8963d588
Geraldine Diaz <geraldine.diaz@mercadolibre.com.co>
2022-09-15 21:03:39 -0500

    add recgnition

--- js/06-app.js
+++ js/06-app.js
@@ -1,46 +1,35 @@
-const salida = document.querySelector('#salida');
-const microfono = document.querySelector('#microfono');
 
- microfono.addEventListener('click', ejecutarSpeechAPI);
+
+const salida = document.querySelector('#salida')
+const microfono = document.querySelector('#microfono')
+
+microfono.addEventListener('click', ejecutarSpeechAPI)
 
 function ejecutarSpeechAPI() {
+    const SpeechRecognition = webkitSpeechRecognition
+    const recgnition = new SpeechRecognition()
 
-    const SpeechRecognition =  webkitSpeechRecognition;
-    const recognition = new SpeechRecognition();
+    recgnition.start()
+    recgnition.onstart = function() {
+        salida.classList.add('mostrar')
+        salida.textContent = 'Escuchando..'
+    }
 
-    // start recognition
-    recognition.start();
+    recgnition.onspeechend = function() {
+        salida.textContent = 'Se dejó de grabar..'
+        recgnition.stop()
+    }
 
+    recgnition.onresult = function(e) {
+        const { confidence, transcript} = e.results[0][0]
+        const speech = document.createElement('p')
+        const seguridad = document.createElement('p')
 
-    // This runs when the speech recognition service starts
-    recognition.onstart = function() {
-        salida.classList.add('mostrar');
-        salida.innerHTML = "Escuchando...";
-    };
-    
-    recognition.onspeechend = function() {
-        salida.innerHTML = "Se detuvo de ejecutar";
-        recognition.stop();
-    };
-  
-    // This runs when the speech recognition service returns result
-    recognition.onresult = function(e) {
+        speech.innerHTML = `Grabado: ${transcript}`
+        seguridad.innerHTML = `Seguridad: ${parseInt(confidence * 100) }%`
+        salida.appendChild(speech)
+        salida.appendChild(seguridad)
 
-        console.log(e.results);
-
-        var transcript = e.results[0][0].transcript;
-        var confidence = e.results[0][0].confidence;
-
-
-        const speech = document.createElement('p');
-        speech.innerHTML = `Grabado: ${transcript}`;
-
-        const seguridad = document.createElement('p');
-        seguridad.innerHTML =  `Seguridad:  ${ parseInt( confidence*100) } %`;
-
-        salida.appendChild(speech);
-        salida.appendChild(seguridad);
-    };
-  
+    }
 
 }
